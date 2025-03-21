e92418a466a3dc93e06e76d3aa719843004e29a9
nithin6743 <nithinkumarsottagari03@gmail.com>
2025-03-21 09:34:33 +0530

    updated nodemodules

--- app.js
+++ app.js
@@ -7,28 +7,34 @@ app.use(cors());
 const port = 3000;
 app.use(express.json());
 
-
 app.post('/add', (req, res) => {
   const body = req.body;
+
   fs.readFile(`${__dirname}/data.json`, 'utf-8', (err, data) => {
     if (err && err.code !== 'ENOENT') {
-      return res.status(500).json({ message: 'Error reading the file !' });
+      return res.status(500).json({ message: 'Error reading the file!' });
     }
 
     let jsonData = data ? JSON.parse(data) : [];
-    jsonData.push({
-      id: jsonData.length + 1,
+
+    jsonData = jsonData.map((item) => ({
+      ...item,
+      id: item.id + 1, 
+    }));
+
+    jsonData.unshift({
+      id: 1,
       data: body,
     });
 
     fs.writeFile(
       `${__dirname}/data.json`,
-      JSON.stringify(jsonData,null,2),
+      JSON.stringify(jsonData, null, 2),
       (err) => {
         if (err) {
-          return res.status(500).json({ message: 'Error writing file !' });
+          return res.status(500).json({ message: 'Error writing file!' });
         }
-        res.status(201).json({ message: 'Data added successfully !' });
+        res.status(201).json({ message: 'Data added successfully!' });
         console.log(jsonData);
       }
     );
@@ -36,6 +42,5 @@ app.post('/add', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
-  
+  console.log(`Server running at http://localhost:${port}`);
+});
